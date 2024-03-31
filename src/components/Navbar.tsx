14dd42873b748a5b82bd916c304d995f92854378
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-03-31 19:51:56 +0900

    ReactでNavbarコンポーネントを更新
    Navbarコンポーネントに、ロゴ、タイトル、MobileNavなどの新しい要素を追加しました。Next.jsアプリのパフォーマンスとSEOを向上させるため、'next/link'と'next/image'を活用しました。Navbarは、レスポンシブデザイン、改良されたレイアウト、追加機能を備えた固定コンポーネントとなりました。

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import MobileNav from '@/components/MobileNav'
+
 const Navbar = () => {
   return (
-      <div>Navbar</div>
+      <nav className={"flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10"}>
+        <Link href={"/"} className={"flex items-center gap-1"}>
+          <Image src={"/icons/logo.svg"} alt={"Zoom logo"} width={32} height={32} className={"max-sm:size-10"} />
+          <p className={"text-[26px] font-extrabold text-white max-sm:hidden"}>
+            Zoom
+          </p>
+        </Link>
+        <div className={"flex-between gap-5"}>
+          <MobileNav />
+        </div>
+      </nav>
   )
 }
 
