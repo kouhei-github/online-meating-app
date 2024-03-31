5a1c7b5742db6ed4b1ee9b562e5f3a917511f26a
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-03-31 19:52:22 +0900

    サイドバー・コンポーネントとglobal.cssユーティリティの更新
    主な変更点は、レイアウト・レンダリングを改善するためのサイドバー・コンポーネントの更新と、global.cssファイルに新しいユーティリティ・クラスを追加したことです。具体的には、サイドバーの誤ったパス名比較が修正され、そのレイアウトがそのコンテンツによりよく対応するように更新されました。新しいユーティリティ・クラスがglobal.cssファイルに追加されました。

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,10 +8,11 @@ import Image from 'next/image'
 const Sidebar = () => {
   const pathname = usePathname()
   return (
-      <section className="sticky left-0 top-0 flex h-scree w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hodden lg:w-[264px]">
-        <div className="flex flex-col gap-6">
+      <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+        <div className="flex flex-1 flex-col gap-6">
           {sidebarLinks.map((link) => {
-            const isActive = pathname === link.route || pathname.startsWith(link.route)
+            const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`)
+
             return (
                 <Link href={link.route} key={link.label} className={cn(
                     "flex gap-4 items-center p-4 rounded-lg justify-start",
