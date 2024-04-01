e3cf34fd47d7c4012d75edf47d2a18b354571153
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-04-01 15:03:27 +0900

    ビデオ通話のためのuseGetCallByIdフックの追加
    アプリケーションでビデオ通話を管理する機能を強化するために、新しいフック useGetCallById が作成された。このフックは、idに基づいて特定のコールの状態を効率的に処理し、コール状態とロード状態の両方を管理します。この新しいフックは、提供されたidを使用して特定のコールをロードおよび追跡するのに役立ち、ビデオコールの管理性と操作性を向上させる。

---
+++ src/hooks/useGetCallByid.ts
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react'
+import {Call, useStreamVideoClient} from '@stream-io/video-react-sdk'
+
+export const useGetCallByid = (id: string | string[]) => {
+  const [call, setCall] = useState<Call>()
+  const [isCallLoading, setIsCallLoading] = useState(true)
+  const client = useStreamVideoClient()
+
+  useEffect( () => {
+    if (!client) return
+    const loadCall = async () => {
+      const {calls} = await client.queryCalls({
+        filter_conditions:{
+          id
+        }
+      })
+      if (calls.length > 0) setCall(calls[0])
+      setIsCallLoading(false)
+    }
+
+    loadCall()
+  }, [client, id] );
+
+  return {call, isCallLoading}
+}
