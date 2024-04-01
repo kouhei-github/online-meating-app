9e44efb2937da1be451f3a445d7a278f38ddd90e
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-04-01 19:58:56 +0900

    ウェブサイトのメタデータの更新とStreamVideoProviderのインポート
    このコミットでは、layout.tsx ファイルのウェブサイトのメタデータ（タイトル、説明、アイコン）を更新し、ルートの layout.tsx ファイルに StreamVideoProvider のインポートを導入しました。これは、全体的なブランディングの更新と、アプリのビデオ通話機能の強化の一環です。

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,14 @@
-"use client"
 import StreamVideoProvider from '@/providers/StreamClientProvider'
 import { Toaster } from "@/components/ui/toaster"
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: "KOHEI MTG",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
 
 
 export default function RootLayout({
