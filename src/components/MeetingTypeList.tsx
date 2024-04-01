2bb346acf1e351d87f66599518b4c784c98c91b1
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-04-01 20:35:54 +0900

    Textareaコンポーネントの追加と依存関係の更新
    UIに新しいTextareaコンポーネントを実装した。この変更の一環として、"@types/react-datepicker "と "react-datepicker "の依存関係が追加され、依存関係"@floating-ui/react "と "date-fns "のバージョンが更新された。この修正は、新しい機能と標準に対応するために行われた。

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -6,6 +6,8 @@ import MeetingModel from '@/components/MeetingModel'
 import {useUser} from '@clerk/nextjs'
 import {Call, useStreamVideoClient} from '@stream-io/video-react-sdk'
 import { useToast } from "@/components/ui/use-toast"
+import {Textarea} from '@/components/ui/textarea'
+import ReactDatePicker from 'react-datepicker'
 
 const MeetingTypeList = () => {
   const { toast } = useToast()
@@ -54,49 +56,95 @@ const MeetingTypeList = () => {
     }
   }
 
-
+  const meetingLisk = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
   return (
-      <section className={"grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4"}>
-        <HomeCard
-            image={"/icons/add-meeting.svg"}
-            title={"New Meeting"}
-            description={"Start an Instant meeting"}
-            handleClick={() => setMeetingState("isInstantMeeting")}
-            className={"bg-orange-1"}
-        />
-        <HomeCard
-            image={"/icons/schedule.svg"}
-            title={"Schedule Meeting"}
-            description={"Plan Your meeting"}
-            handleClick={() => setMeetingState("isScheduleMeeting")}
-            className={"bg-blue-1"}
-        />
-        <HomeCard
-            image={"/icons/recordings.svg"}
-            title={"View Recordings"}
-            description={"Check out your recordings"}
-            handleClick={() => setMeetingState("isJoiningMeeting")}
-            className={"bg-purple-1"}
-        />
-        <HomeCard
-            image={"/icons/join-meeting.svg"}
-            title={"Join Meeting"}
-            description={"via invitation link"}
-            handleClick={() => setMeetingState("isJoiningMeeting")}
-            className={"bg-yellow-1"}
-        />
-
+    <section className={"grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4"}>
+      <HomeCard
+          image={"/icons/add-meeting.svg"}
+          title={"New Meeting"}
+          description={"Start an Instant meeting"}
+          handleClick={() => setMeetingState("isInstantMeeting")}
+          className={"bg-orange-1"}
+      />
+      <HomeCard
+          image={"/icons/schedule.svg"}
+          title={"Schedule Meeting"}
+          description={"Plan Your meeting"}
+          handleClick={() => setMeetingState("isScheduleMeeting")}
+          className={"bg-blue-1"}
+      />
+      <HomeCard
+          image={"/icons/recordings.svg"}
+          title={"View Recordings"}
+          description={"Check out your recordings"}
+          handleClick={() => setMeetingState("isJoiningMeeting")}
+          className={"bg-purple-1"}
+      />
+      <HomeCard
+          image={"/icons/join-meeting.svg"}
+          title={"Join Meeting"}
+          description={"via invitation link"}
+          handleClick={() => setMeetingState("isJoiningMeeting")}
+          className={"bg-yellow-1"}
+      />
+      {!callDetails ? (
         <MeetingModel
-          isOpen={meetingState === "isInstantMeeting"}
+          isOpen={meetingState === "isScheduleMeeting"}
           onClose={() => setMeetingState(undefined)}
-          title={"Start an Instant Meeting"}
-          className={"text-center"}
-          buttonText={"Start Meeting"}
+          title={"Create Meeting"}
           handleClick={() => createMeeting()}
+        >
+          <div className={"flex flex-col gap-2.5"}>
+            <label htmlFor="" className=" text-base text-normal leading-[22px] text-sky-2">
+              Add a description
+            </label>
+            <Textarea
+              className={"border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"}
+              onChange={(e) => setValues({...values, description: e.target.value})}
+            />
+          </div>
+          <div className={"flex w-full flex-col gap-2.5"}>
+            <label htmlFor="" className=" text-base text-normal leading-[22px] text-sky-2">
+              Select Date and Time
+            </label>
+            <ReactDatePicker
+              onChange={(date) => setValues({...values, dateTime: date!})}
+              selected={values.dateTime}
+              showTimeSelect
+              timeFormat={"HH:mm"}
+              timeIntervals={15}
+              timeCaption={"time"}
+              dateFormat={"MMMM d, yyyy h:mm aa"}
+              className={"w-full rounded bg-dark-3 p-2 focus:outline-none"}
+            />
+          </div>
+        </MeetingModel>
+      ) : (
+        <MeetingModel
+          isOpen={meetingState === "isScheduleMeeting"}
+          onClose={() => setMeetingState(undefined)}
+          title={"Meeting Created"}
+          className={"text-center"}
+          buttonText={"Copy Meeting Link"}
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLisk);
+            toast({title: "Link copied"})
+          }}
+          image={"/icons/checked.svg"}
+          buttonIcon={"/icons/copy.svg"}
         />
+      )}
+      <MeetingModel
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title={"Start an Instant Meeting"}
+        className={"text-center"}
+        buttonText={"Start Meeting"}
+        handleClick={() => createMeeting()}
+      />
 
 
-      </section>
+    </section>
   )
 }
 
