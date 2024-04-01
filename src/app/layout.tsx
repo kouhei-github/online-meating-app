00b0556ef68eff5094df1e6487c791336273cb83
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-04-01 15:04:08 +0900

    Stream ビデオチャット SDK スタイルのインポートの追加
    このコミットでは、レイアウトファイルに Stream ビデオチャット SDK スタイルのインポートが追加されました。これにより、アプリケーション内のビデオチャット機能のレイアウトと外観が改善されます。このインポートにより、ビデオチャット機能の視覚的なインタラクティブ要素は SDK の標準スタイルガイドラインに沿ったものになります。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
 
+import "@stream-io/video-react-sdk/dist/css/styles.css"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
